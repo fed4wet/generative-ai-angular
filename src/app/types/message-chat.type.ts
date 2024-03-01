0eadbd8fa5a9486713aae1718c5f416d514dd494
Oleh Sypiahin <fed4wet@gmail.com>
2024-03-01 16:38:51 +0200

    Code Cleaning: Update Angular to 17.2

--- src/app/types/message-chat.type.ts
+++ src/app/types/message-chat.type.ts
@@ -1,7 +1,7 @@
 export interface ChatMessage {
-  id: string,
-  text: string,
-  sender: string,
-  avatar: string,
-  isRaw?: boolean,
+  id: string;
+  text: string;
+  sender: string;
+  avatar: string;
+  isRaw?: boolean;
 }
